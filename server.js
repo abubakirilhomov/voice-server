e20d4a6a45734570ee3a519acd621ac124868f6a
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-12-14 18:21:54 +0500

    sadsada

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Configure Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://voice-front-three.vercel.app"], // Frontend origins
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://voice-front-three.vercel.app"], // Frontend origins
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   },
